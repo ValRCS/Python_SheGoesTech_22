8475518addf8357a0fd23828a7a4a3655e9ad200
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-08 20:25:03 +0300

    Variables and input

--- Day_2_Variables_Data_Types/day2_variables.py
+++ Day_2_Variables_Data_Types/day2_variables.py
@@ -2,53 +2,62 @@
 # print("Valdis", "Zatlers")  # i can use comma to separate multiple objects
 # print("Valdis", "Zatlers", "Python")
 # # we can do math in Python
-print(2 + 2)
-# print(2 - 2)
-# print(2 * 3)
-# print(2 / 2) # result is float, decimal number with comma
-# print(2 ** 5)  # 2 to the power of 5
-# print(2 ** 0.5)  # 2 to the power of 0.5 that is square root
-# print(10 % 3)  # modulo operator, remainder of division, so here it is 1
-# print googol
-print(10 ** 100) # 10 to the power of 100, Python supports large numbers out of the box
+# print(2 + 2)
+# # print(2 - 2)
+# # print(2 * 3)
+# # print(2 / 2) # result is float, decimal number with comma
+# # print(2 ** 5)  # 2 to the power of 5
+# # print(2 ** 0.5)  # 2 to the power of 0.5 that is square root
+# # print(10 % 3)  # modulo operator, remainder of division, so here it is 1
+# # print googol
+# print(10 ** 100) # 10 to the power of 100, Python supports large numbers out of the box
 
-# why we had to move from 32 bits
-print(2 ** 32)  # 4294967296 so this is famous 32 bit limit
-print(2 ** 64)  # 18446744073709551616
+# # why we had to move from 32 bits
+# print(2 ** 32)  # 4294967296 so this is famous 32 bit limit
+# print(2 ** 64)  # 18446744073709551616
 
-# for math we can use parenthesis
-print((2 + 3) * 4)  # 20
+# # for math we can use parenthesis
+# print((2 + 3) * 4)  # 20
 
-print("beer " * 5)  # beer beer beer beer beer
-print("Valdis" + " likes " + "beer")  # Valdis likes beer
+# print("beer " * 5)  # beer beer beer beer beer
+# print("Valdis" + " likes " + "beer")  # Valdis likes beer
 # # # # we declare variables in Python when we first use them
-# my_name = "Valdis"  # there are no types no val, no var no const nothing like that in Python
-# print(my_name)
-# print("my_name")  # this is not a variable, this string literal
+my_name = "Valdis"  # there are no types no val, no var no const nothing like that in Python
+print(my_name) # this prints Valdis
+print("my_name")  # this is not a variable, this string literal
+print(my_name + " likes beer")  # Valdis likes beer
+# i can keep using my_name
+print(my_name + " likes beer" * 5)  # Valdis likes beer beer beer beer beer
 # #
 # # # variables do have data types in Python
-# # print(type(my_name))
+print(type(my_name))
 # # # so for any variables you can find out type with type(my_variable)
 # # # there are primitive types and compound(collection) types
-# my_num = 42
-# print(my_num)
-# print(type(my_num))
-# a = 42
-# b = 42
-# print(id(a), id(b), id(my_num))
-# print(a, b, my_num)
-# b = 9001  # b now points to a different memory location
-# c = 9_000  # you can use underscore _ for larger numbers just as a cosmetic , machine ignore it starting from python 3.6
+my_num = 42  #answer to everything
+print(my_num)
+print(type(my_num))  # should be int
+a = 42
+b = 42
+c = 43
+print(id(a), id(b), id(c), id(my_num))
+print(id(c)-id(b)) # so storing int took 32 bits or 4 bytes
+# id shows where in memory(virtual memory) the variable is stored
 
-# my_result = b + c  # new variable my_result is introduced
-# print(b, c, my_result)
+print(a, b, my_num)
+# you can reassign variables
+b = 9001  # b now points to a different memory location
+c = 9_000  # you can use underscore _ for larger numbers just as a cosmetic , machine ignore it starting from python 3.6
+print(a, b, c, my_num)
+my_result = b + c  # new variable my_result is introduced
+print(b, c, my_result)
 # #
 # print(id(b))  # this shows where in virtual memory the b points at
 # print(id(c))  # this shows where in virtual memory the b points at
 # #
-# print(my_name, type(my_name))
-# my_name = 7  # so here dynamically Python changed where my_name points at
-# print("I do not want ot be a number!", my_name, type(my_name))  # so now my_name points to different content
+print(my_name, type(my_name))  # my_name is a string
+# you can change the type of variable, but generally it is best avoided
+my_name = 7  # so here dynamically Python changed where my_name points at
+print("I do not want ot be a number!", my_name, type(my_name))  # so now my_name points to different content
 # # # but also my_name is now pointing to different data type all together
 # #
 # # # this is convenient at times
@@ -62,57 +71,81 @@ print("Valdis" + " likes " + "beer")  # Valdis likes beer
 # # # so Python really shines in prototypes, smaller projects
 # #
 # # # there are exceptions, like Dropbox which has those 3 million lines of Python :)
-# my_name = "Valdis"  # i am back to normal
-# president = "Valdis"
-# neighbor = "Valdis"
-# print(id(my_name), id(president), id(neighbor), sep="\n")  # sep is what to use between , in print
-# neighbor = "Voldemars"
-# print(id(my_name), id(president), id(neighbor), sep="\n***\n")
+my_name = "Valdis"  # i am back to normal
+president = "Valdis"
+neighbor = "Valdis"
+print(id(my_name), id(president), id(neighbor), sep="\n")  # sep is what to use between , in print
+neighbor = "Voldemars"
+print(id(my_name), id(president), id(neighbor), sep="\n***\n")
 # #
 # neighbor = "VisValdis"  # Voldemars is going to be garbage collected, otherwise we would get a memory leak
-# print(c)  #
-# c = c + 5000  # this is not algebra we are creating a new value on the left from the right
+print(c)  #
+c = c + 5000  # this is not algebra we are creating a new value on the left from the right
+# so we added 5000 to c and assigned it to c
 # # # evaluation happens from the right side , assignment to the left
 # # # above is not an equality!! there is another symbol for equality (Day 3)
-# print(c)
-# #
-# c += 10_000  # shorter way of writing c = c + 10000
-# print(c, type(c))
+print(c)  # this should be 14000
+# # this c = c + 5000 is so common that there is a shortcut
+c += 10_000  # shorter way of writing c = c + 10000  # computer speed wise it is the same
+print(c, type(c))
 
-# c -= 5_000  # shorter way of writing c = c - 5000
-# print(c, type(c))
+c -= 5_000  # shorter way of writing c = c - 5000
+print(c, type(c))
 # #
-# c = c + 3.1415926  # this is a float that we are adding to c
-# print(c, type(c))  # so c changed data type to float
+c = c + 3.1415926  # this is a float that we are adding to c
+print(c, type(c))  # so c changed data type to float
 # #
-# c = c - 3.1415926  # this is a float that we are subtracting from c
-# print(c, type(c))  # still float
+c -= 3.1415926  # this is a float that we are subtracting from c
+print(c, type(c))  # still float
 
 # # what do to if we want an integer?
-# c = int(c)  # this is a conversion, so called type casting
-# print(c, type(c))  # back to integer
+c = int(c)  # this is a conversion, so called type casting
+print(c, type(c))  # back to integer
 
-# e = 2.718281828459045  # this is a float
-# print(e, type(e))
-# e_rounded = round(e)  # this is a rounding function default will be integer with no extre arguments
-# print(e_rounded, type(e_rounded))
-# e_rounded_to_2 = round(e, 2)  # this is a rounding function with 2 decimals
-# print(e_rounded_to_2, type(e_rounded_to_2))
+
+e = 2.718281828459045  # this is a float
+print(e, type(e))
+flat_e = int(e)  # this is a conversion, so called type casting
+print(e, flat_e)
+e_rounded = round(e)  # this is a rounding function default will be integer with no extre arguments
+print(e_rounded, type(e_rounded))
+e_rounded_to_2 = round(e, 2)  # this is a rounding function with 2 decimals
+print(e_rounded_to_2, type(e_rounded_to_2))
 # print(e_rounded_to_2)
+# so 2.5 will be rounded to 2, 3.5 will be rounded to 4
+print(round(2.49), round(2.5), round(2.50000000001), round(2.51))
+print(round(3.5)) # so what is going on? 3.5 is halfway between 3 and 4
+print(round(4.5))
+print(round(5.5))
+print(round(6.5))
+print(round(7.5))
+print(round(8.5))  # when we learn loops this will be easier to write :)
+print(3.5 - 3 - 0.5)
+# problem with floats is that they are not exact
+print(0.1 + 0.2)  # 0.30000000000000004
+my_round_3 = round(0.1 + 0.2, 6)  # 0.3
+print(my_round_3)
 
-# # e = int(e)  # this is a conversion, so called type casting, here it works as floor function
-# # print(e, type(e))  # back to integer, so we lose some information
+print(round(e, 6))  # 2.718282
+e = int(e)  # this is a conversion, so called type casting, here it works as floor function
+print(e, type(e))  # back to integer, so we lose some information
 # #
 # # my_pi = 3.1415926
 # # print(my_pi, type(my_pi))
 # #
 # # # Naming things are hard
+# you can use letters, numbers and underscore _ but not at the beginning
 # # #Function and Variable Names
 # # # from https://www.python.org/dev/peps/pep-0008/#prescriptive-naming-conventions
 # # # Function names should be lowercase, with words separated by underscores as necessary to improve readability.
 # # #
 # # # Variable names follow the same convention as function names.
 # # #
+my_cool_variable = 5555 # this is called snake_case, Python standard
+myCoolVariable = 5555 # this is called camelCase, Java standard
+# do not use BOTH in the same project
+my_variable_2 = 5555 # so numbering is fine but if you have more than few numbers you might need 
+# different data structure like list or tuple (explained later)
 # # # mixedCase is allowed only in contexts where that's already the prevailing style (e.g. threading.py), to retain backwards compatibility.
 # #
 # # bad would be to have my_name and myName in a same project
@@ -144,32 +177,33 @@ print("Valdis" + " likes " + "beer")  # Valdis likes beer
 # # # # why? because l can be confused with 1 and I on some systems
 # # #
 # # # # all capital variable means that it really is a constant
-# # RTU = "Riga Technical University"
-# # print(RTU)
-# # RTU = "Tieto" # no body is going to stop you here but you should not do this if you have capital letters
-# # print(RTU)
+RTU = "Riga Technical University"
+print(RTU)
+RTU = "Accenture" # no body is going to stop you here but you should not do this if you have capital letters
+print(RTU)
 # # # # we shouldn't change this
 # # #
 # # # # we use English
-# # kaķis = "my cat"  # please do not do this :)
-# # print(kaķis)
-# # my_cat = "mans kaķis Muris murrā" # that is fine
-# # print(my_cat)
-# # # smiley_msg = "AHmm ! 😀 -> \u1F600" # this is 16bit Unicode and 0
-# # # print(smiley_msg)
-# # smiley_again = "Aha 😆 ! 😀 -> \U0001F600" # for Unicode encoded with more than 4 Hex symbols you need to use 32
-# # print(smiley_again)
+kaķis = "my cat"  # please do not do this :)
+print(kaķis)
+my_cat = "mans kaķis Muris murrā" # that is fine
+print(my_cat)
+smiley_msg = "AHmm ! 😀 -> \u1F600" # this is 16bit Unicode and 0
+print(smiley_msg)
+smiley_again = "Aha 😆 ! 😀 -> \U0001F600" # for Unicode encoded with more than 4 Hex symbols you need to use 32
+print(smiley_again)
 # #
 # # # so we have str, int, and float so far
 # # # there is also boolean (Day 3), None -> which is specific type for representing nothing
 # # # then we have more complext collections
 # #
-# # my_str = "888.67" # this is str
-# # my_float = float(my_str) # this is forced type casting we want to cast our text string into a number
-# # my_int = int(my_float) # here I cast float to integer so naturally I lose everything after comma
-# #
-# # print(my_str, my_float, my_int)
+my_str = "888.67" # this is str
+my_float = float(my_str) # this is forced type casting we want to cast our text string into a number
+my_int = int(my_float) # here I cast float to integer so naturally I lose everything after comma
+
+print(my_str, my_float, my_int)
 # #
 # # # not everthin will cast to number
-# # print(my_name)
-# # # will_fail = int(my_name) # this will fail with an ValueError # there are ways of checking
+print(my_name)
+# will_fail = int(my_name) # this will fail with an ValueError # there are ways of checking
+# we will need to handle Exceptions later in the course
--- Day_2_Variables_Data_Types/handling_input.py
+++ Day_2_Variables_Data_Types/handling_input.py
@@ -2,26 +2,27 @@ import datetime
 print("Today is", datetime.datetime.now())
 print("The year is", datetime.datetime.now().year)
 
-# my_name = input("What is your name friend? ")
-# print(f"Wow that is a nice name {my_name}") # i am using f-string formatting which is available in Python since 3.6 and up
+my_name = input("What is your name friend? ")
+print(f"Wow that is a nice name {my_name}") # i am using f-string formatting which is available in Python since 3.6 and up
+# old alternative using +
+print("Wow that is a nice name " + my_name)
 # # # # this lets me put variable directly into my string text
 # # # # otherwise I would have to do
 # print("Wow that is a nice name " + my_name) # same as above but gets tricky for longer strings
 # # #
-# a = input("what number you want to double? ")
-# double_str_a = a * 2 # a is a string so we need to convert it to an int
-# print(f"Cool {a} doubled is {double_str_a}, or is it ?")
+a = input("what number you want to double? ")
+double_str_a = a * 2 # a is a string so we need to convert it to an int
+print(f"Cool {a} doubled is {double_str_a}, or is it ?")
 # # # #solution is to cast to the type we want
-# my_float = float(a) # is should do this before int cast because casting to int would lose the values
-# float_square = my_float**2
-# print(f"Cool {my_float} squared is {float_square}, or is it ?")
-# a = int(my_float) # so a became an integer # we could also use float(a) if we want floats
-# double_a = a * 2
-# print(f"Cool {a} doubled is {double_a}, or is it ?")
+my_float = float(a) # is should do this before int cast because casting to int would lose the values
+float_square = my_float**2
+print(f"Cool {my_float} squared is {float_square}, or is it ?")
+a = int(my_float) # so a became an integer # we could also use float(a) if we want floats
+double_a = a * 2
+print(f"Cool {a} doubled is {double_a}, or is it ?")
 #
-# a = 3.1415926
-# round_a_4_digits = round(a, 4) # 4 digits after comma
-# print(a, round_a_4_digits, round(a, 2), round(a))
+rounded_float = round(float_square, 4)
+print(f"Rounded to 4 decimals {rounded_float}")
 #
 # my_result = 0.1+0.2
 # print(my_result)  # floats are not precise so this is not 0.3
