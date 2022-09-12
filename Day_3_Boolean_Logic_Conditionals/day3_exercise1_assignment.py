4265d9eb4946a72b25bef557f1601fb347bf085b
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-12 20:29:40 +0300

    update exercise text

--- Day_3_Boolean_Logic_Conditionals/day3_exercise1_assignment.py
+++ Day_3_Boolean_Logic_Conditionals/day3_exercise1_assignment.py
@@ -6,6 +6,7 @@
 
 # If 35 to 37 (inclusive), output "all right"
 
-# If the temperature  over 37, then output  "possible fever
+# If the temperature  over 37, then output  
+# "possible fever
 
 # remember about type conversion if needed
