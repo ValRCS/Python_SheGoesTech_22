85ce8e05b0b39f5bfefa38406f45ba75d03bdcf4
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-13 22:02:28 +0300

    More commits

--- Day_4_Loops/day4_exercise_2.py
+++ Day_4_Loops/day4_exercise_2.py
@@ -14,3 +14,20 @@
 
 # Note: remember that several symbols can be 
 # printed at once, for example: print ("" * 10 + "*" * 6)
+
+tree_height=int(input("Please enter height of the tree"))
+
+print(tree_height)
+stars=1
+
+for i in range(1,tree_height+1):
+    print(" " * (tree_height-i) +  "*" * stars)
+    stars=stars+2
+    # so improvement would be to calculate stars from i
+    # stars = i*2-1
+
+for i in range(1,tree_height+1):
+    print(" " * (tree_height-i) +  "*" * (i*2-1))
+
+
+
--- Day_4_Loops/day4_exercise_3.py
+++ Day_4_Loops/day4_exercise_3.py
@@ -3,3 +3,33 @@
 #  A prime number is a number that divides without remainder only by itself and 1.
 
 # Hint: what numbers do we have to check?
+
+ # 3. Primes Check if entered positive number is a prime number.
+
+#  A prime number is a number that divides without remainder only by itself and 1.
+
+# Hint: what numbers do we have to check?
+
+n=int(input("Please enter a positive number to check if it is prime "))
+# is_prime=True # so flag based approach
+
+# for i in range(2,n):
+#     if n%i == 0 and i !=n:
+#         print(f"Sorry {n} is not a prime number because it divides by {i}")
+#         is_prime=False
+#         break # no need to check further
+
+# if is_prime is False:
+#     print(f"{n} is not a prime number")
+# else:  
+#     print(f"{n} is a prime number")
+
+
+# for i in range(2, n): # so n-1 is clearly sufficient but can we do better?
+for i in range(2, int(n**0.5)+1): # so we check only up to and including square root of n
+    if n % i == 0:
+        print(f"{n} is not a prime number.")
+        print(f"Sorry {n} is not a prime number because it divides by {i}")
+        break
+else: # this is executed if the for loop is not broken out of with break
+    print(f"I am sure that {n} is a prime number!")
