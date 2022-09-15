a44d671819c64cb0570f183f03f7a5fa16a7ac85
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-15 22:02:30 +0300

    Weekend Exercise until Tuesday

---
+++ Day_5_Strings/day5_weekend_homework.py
@@ -0,0 +1,15 @@
+# write a program that asks for two text inputs s1 and s2 
+# you can use better names than s1 and s2
+
+# then checks if all the characters in first string are in second string
+# if they are, print All character counts in the second string
+# if not, print Not all characters are in the second string
+# example
+# s1 = "abc"
+# s2 = "abracadabra"
+# output: a 5, b 2, c 1, d 1, r 2  # so do not print the empty ones
+
+# example two
+# s1 = "abc"
+# s2 = "def"
+# output: Not all characters are in the second string
