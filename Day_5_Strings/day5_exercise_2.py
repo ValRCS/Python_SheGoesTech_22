d9265604db2a5b9570e32ac909187ca1a8ecc81d
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-15 20:16:26 +0300

    strings with exercises

---
+++ Day_5_Strings/day5_exercise_2.py
@@ -0,0 +1,26 @@
+# 2. Almost Hangman
+
+# Write a program to recognize a text symbol
+
+# The user (first player) enters the text.
+
+# Only asterisks instead of letters are output.
+
+# Assume that there are no numbers, but there may be spaces.
+
+# The user (i.e. the other player) enters the symbol.
+
+# If the letter is found, then the letter is displayed in ALL the appropriate places, all other letters remain asterisks.
+
+# Example:
+
+# First input: Kartupeļu lauks -> ********* *****
+
+# Second input: a -> *a****** *a***
+
+
+
+
+# In principle, this is a good start to the game of hangman.
+
+# https://en.wikipedia.org/wiki/Hangman_(game)
---
+++ Day_5_Strings/day5_exercise_3.py
@@ -0,0 +1,26 @@
+# 3. Text conversion
+
+# Write a program for text conversion
+
+# Save user input
+
+# Print the entered text without changes
+
+# Exception: if the words in the input are not .... bad, 
+# then the output is not ...  bad section must be changed to is good
+
+
+
+# Examples:
+
+# The weather is not bad -> The weather is good
+
+# The car is not new -> The car is not new
+
+# This cottage cheese is not so bad -> This cottage cheese is good 
+
+# Hints:
+
+# Find (or index, or even rfind) will probably come in handy, as may an operator. Also slice syntax will be useful.
+
+# Extra: How would you do this task in Latvian language (nav slikts/a -> ir labs/a)?
