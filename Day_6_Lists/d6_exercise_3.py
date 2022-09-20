b95185e96e95f39dbaa4d4964d5705aafff66191
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-20 21:58:12 +0300

    Day 7 exercises including take home one

--- Day_6_Lists/d6_exercise_3.py
+++ Day_6_Lists/d6_exercise_3.py
@@ -11,3 +11,19 @@
 # notice how each word was reversed separately
 
 # PS Split and join operations could be useful here.
+
+sentence = input('Enter a sentence, please: ')
+words = sentence.lower().split()
+
+# reversed_words=[]
+# for word in words:
+#     reversed_words.append(word[::-1])
+# same as above
+reversed_words = [word[::-1] for word in words]
+# capitalize the first letter of first word
+reversed_words[0] = reversed_words[0].capitalize()
+# create a new string from the list
+new_sentence = ' '.join(reversed_words)
+
+#maybe I should check the uppercase...
+print(new_sentence)
