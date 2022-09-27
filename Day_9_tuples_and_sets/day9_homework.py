f8f36543d1a071a62de6f6d7873c3fb78a3adcd9
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-27 22:19:18 +0300

    homework day 10

---
+++ Day_9_tuples_and_sets/day9_homework.py
@@ -0,0 +1,16 @@
+# write a function of 3 arguments all strings
+# function should return lexigraphically ordered string of unique characters
+# these unique characters must be present in BOTH  of the first two strings 
+# BUT not in the third "badstring"
+
+# example:
+# "abcf", "fab", "boo"  returns -> "af" 
+# because "a" is in both "abc" and "fab" but not in "boo"
+# same goes for "f" it is present in both "good strings" but not in "badstring"
+# notice "b" is not in the result because it is in the badstring.
+
+# slightly harder way would be to use loops and if statements to check each character
+# the easy way is to use sets and set operations 😊
+
+# either approach is acceptable
+
