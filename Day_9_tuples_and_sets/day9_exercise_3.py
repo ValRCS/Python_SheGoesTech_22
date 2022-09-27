6153f21031a65e861c165f524fad485956d80efb
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-27 22:07:35 +0300

    sets tuples exercises

--- Day_9_tuples_and_sets/day9_exercise_3.py
+++ Day_9_tuples_and_sets/day9_exercise_3.py
@@ -6,13 +6,9 @@
 # We return False otherwise
 
 
-
 # pangram - sentence, word string containing all letters of the alphabet - https://en.wikipedia.org/wiki/Pangram
 
 # We ignore spaces and believe that uppercase is as valid as lowercase, i. here A and a -> a
-
-
-
 
 # print(is_pangram("The five boxing wizards jump quickly")) -> True
 # print(is_pangram("Not a pangram")) -> False
@@ -27,3 +23,27 @@
 
 # some languages have perfect pangrams, some do not
 # perfect pangram - a pangram that uses every letter of the alphabet at just once
+
+# def is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz'):
+#     text_set = set(text.lower())
+#     abc_set = set(alphabet)
+#     return abc_set.intersection(text_set) == abc_set
+
+# def is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz'):
+#     spl_text = set(text.lower())
+#     spl_alph = set(alphabet)
+#     # return spl_alph.issubset(spl_text)
+#     return spl_alph <= spl_text # same as above
+
+# one line function
+def is_pangram(text, alphabet="abcdefghijklmnopqrstuvwxyz"):
+        return set(alphabet).issubset(set(text))
+
+print(is_pangram("The five boxing wizards jump quickly"))
+print(is_pangram("Not a pangram"))
+
+a_lt="aąbcčdeęėfghiįyjklmnoprsštuųūvzž"
+print(is_pangram("Įlinkdama fechtuotojo špaga sublykčiojusi pragręžė apvalų arbūzą", alphabet=a_lt)) #true
+# translate Įlinkdama fechtuotojo špaga sublykčiojusi pragręžė apvalų arbūzą to English
+# https://translate.google.com/?sl=lt&tl=en&text=Įlinkdama%20fechtuotojo%20špaga%20sublykčiojusi%20pragręžė%20apvalų%20arbūzą&op=translate
+
