6d436987e217338d7136c5c0780e5545ecfb36f7
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-27 20:11:41 +0300

    Day 10 Set operations

--- Day_9_tuples_and_sets/sets.py
+++ Day_9_tuples_and_sets/sets.py
@@ -1,135 +1,154 @@
-# # # # # # # # # # Sets (kopas)
-# # # # # # # # # # unordered
-# # # # # # # # # # unique items
-# # # # # # ### mutable
-# # # # # # # # # # use: get rid of duplicates,
-# # # membership testing - faster than list or tuple
-# # # # # # # # # # https://docs.python.org/3/tutorial/datastructures.html#sets
-# # # # # # # # # # https://realpython.com/python-sets/
-# #
+# # # # # # # # # # # Sets (kopas)
+# how do you say "set" in Lithuanian?
+# 
+# # # # # # # # # # # unordered - means there is no specific order guarantee
+# # # # # # # # # # # unique items
+# # # # # # # ### mutable - just like lists and dictionaries
+# # # # # # # # # # # use: 
+# 1. get rid of duplicates,
+# # # # 2. membership testing - faster than list or tuple
+# in some ways you could think of set as a dictionary without values - just keys
+# # # # # # # # # # # https://docs.python.org/3/tutorial/datastructures.html#sets
+# # # # # # # # # # # https://realpython.com/python-sets/
+# # #
 chars = set("abracadabra")  # you pass an iterable to set(iterablehere)
 print(chars)
 print(len(chars))
 print('a' in chars) # set membership check will be O(1) so faster than in lists and tuples
+# just like checking for keys in dictionaries is very fast even in a large dictionary
 print('e' in chars)
 print('bl' in chars)
+print(9000 in chars)
 print(str(chars), "not quite what we wanted") # not quite what we need
 print("".join(chars)) # so with join we will create a string from set
-# print(sorted("".join(chars))) # not quite
-# # finally
+# # print(sorted("".join(chars))) # not quite
+# # # finally
 print("".join(sorted(chars))) # so use this if we want sorted unique characters from our string
-# # # print(list(chars))
-# # # # # # # # # # # # # # recipe to sort characters unique characters into a list an then make a string
-# # print("".join(sorted(set("abraValdiscadbra")))) # we staret from inner parenthesis
-# # # # # # # # # # # str(chars)  # not much point in this one
-words = ['potato', 'potatoes', 'tomato', 'Valdis', 'potato']
+print(list(chars)) # often times we want to convert to list after creating a set
+# # # # # # # # # # # # # # # recipe to sort characters unique characters into a list an then make a string
+# # # print("".join(sorted(set("abraValdiscadbra")))) # we staret from inner parenthesis
+# # # # # # # # # # # # str(chars)  # not much point in this one
+words = ['potato', 'potatoes', 'tomato', 'Valdis', 'potato', 'tomato']
 unique_words = set(words)
 print(unique_words)
-# # print(unique_words[1]) # 'set' object is not subscriptable
+# print(unique_words[1]) # 'set' object is not subscriptable
 list_unique_words = list(unique_words)
 print(list_unique_words)
-# print(set(words))
-# print(list(set(words)))
-# # print(list_unique_words[1])
-# #
-# # # # # # # # # # # # difference between set() and {setitem}
+# # print(set(words))
+# # print(list(set(words)))
+# # # print(list_unique_words[1])
+# so recipe for unique words in a list
+list_of_unique_words = list(set(words))
+print(list_of_unique_words)
+# # #
+# # # # # # # # # # # # # difference between set() and {setitem}
 new_set = {'kartupelis'} # so not a dictionary but set
 print(new_set)
 food_item_set = set(['kartupelis']) # same as above
 print(food_item_set)
 chars = set("kartupelis")  # you pass an iterable to set(iterablehere)
 print(chars)  #notice no specific order
-# #
-# # # # # # # Set operations
-print(new_set == food_item_set, new_set is food_item_set)
-# # # # # # # # # # above two sets are equal but are two distinct data sets
-# #
-# #
-numbers = set(range(12))
+# # #
+# # # # # # # # Set operations
+print(new_set == food_item_set, new_set is food_item_set) # so contents are the same but they are different objects
+# # # # # # # # # # # above two sets are equal but are two distinct data sets
+# # #
+# # #
+numbers = set(range(12)) # range is iterable, so we can pass it to set
 print(numbers) # might print in order but the order is not guaranteed
 n3_7 = set(range(3, 8))
 print(n3_7)
+# membership testing
 print(4 in numbers)
 print(4 in n3_7)
-print(numbers.issubset(numbers))
-# # # # # # # # # # # below two are equal
+# we can check whether one set is a subset of another
+print(numbers.issubset(numbers)) # can a set be a subset of itself?
+# # # # # # # # # # # # below two are equal
 print(n3_7.issubset(numbers))
 print(n3_7 <= numbers)  # same as above, this lets you have equal sets
 print("n3_7 < numbers ", n3_7 < numbers ) # this will be false if both sets are equal
 print("numbers < numbers", numbers < numbers) # this should be false
 n_8_15 = set(range(8,16))
 print(n_8_15 < numbers) # False because not all of n_8_15 is in numbers
-# # # # # # # # # # numbers <= numbers
-# # # # # # # # # # numbers < numbers
-# # # # # # # # # # # below two are euqal
+# # # # # # # # # # # numbers <= numbers
+# # # # # # # # # # # numbers < numbers
+# # # # # # # # # # # # below two are euqal
+# we can go the other way
+# that is check if a set is a superset of another
 print(numbers.issuperset(n3_7))
 print(numbers >= n3_7) # same as above just shorter
 print(numbers > n3_7)
 print("numbers > numbers", numbers > numbers) # this should be false
-# #
+# # #
 n5_9 = set(range(5, 10))
 print(n5_9)
-# # # # # # # # # # below two are equal
-print(n3_7 | n5_9)
-print(n3_7.union(n5_9))
+# # # # # # # # # # # below two are equal
+print(n3_7 | n5_9) # so union of two sets with no duplicates
+print(n3_7.union(n5_9)) # same as above
 n3_9 = n3_7 | n5_9  # i could chain union more sets here
 print(n3_9)
-# print(type(n3_9))
+# # print(type(n3_9))
 print(n3_7 | n5_9 | set(range(30, 35))) # i can chain |
-# # # # # # # # # # # below two are equal
-# in set intersection we have to have the same items in both sets
-n5_7 = n3_7 & n5_9
+# # # # # # # # # # # # below two are equal
+# # in set intersection we have to have the same items in both sets
+n5_7 = n3_7 & n5_9 # so & is intersection, analogous to and
 print(n3_7.intersection(n5_9))
 print(n5_7)
-print(n3_7 & n5_9 & {7,22,56,1,5,4,7})
+print(n3_7 & n5_9 & {7,22,56,1,5,4,7}) # so 6 will be gone
 print(n3_7 & n5_9 & {7,22,56,1,5,4,7} & set([5]))  # only 5 is in all 4 sets
-# #
+# careful with empty sets
+print(n3_7 & n5_9 & {7,22,56,1,5,4,7} & set([5]) & set())  # intersection with empty set is empty set
+# # #
+# with set difference we get items that are in the first set but not in the second 
 n3_4 = n3_7 - n5_9  # this is not symmetrical
 print(n3_4) # only 3 and 4 are in n3_7 but not in n5_9
 n8_9 = n5_9 - n3_7  # this is not symmetrical
 print(n8_9, n5_9.difference(n3_7))  # so - is shorthand for difference, as long as values are sets
-# # # # # # # # # # # symmetrical difference
-# in symmetrical difference we need item in one set but not the other
+# # # # # # # # # # # # symmetrical difference
+# # in symmetrical difference we need item in one set but not the other
+# so unique items in both sets, not shared by both
 n_sim = n3_7 ^ n5_9
 print(n_sim, n3_7.symmetric_difference(n5_9))
-# #
+# # #
+# we can check whether two sets are disjoint that is not sharing any items
 print(n3_4.isdisjoint(n8_9)) # are sets completely disjoint(different) nothing in common
-# #
+# # #
 print(n_sim)
-n_sim.update([4, 1, 6, 1, 6, 13, 2]) # so n_sim update is IN PLACE
+n_sim.update([4, 1, 6, 1, 6, 13, 2]) # so n_sim update is IN PLACE, so a bit like union, but we can use any iterable
 print(n_sim)
 n_sim.update([-5, 5, 3, 6, 6, 8, 99, 9, 9])
 print(n_sim)
 n_sim.remove(13)  # Raises KeyError if you try to remove nonexistant value
 print(n_sim)
-# # # n_sim.remove(13)  # Raises KeyError if you try to remove nonexistant value
-# # # print(n_sim)
-n_sim.discard(13)  # No error
+# # # # n_sim.remove(13)  # Raises KeyError if you try to remove nonexistant value
+# # # # print(n_sim)
+n_sim.discard(13)  # No error, will do nothing if you try to remove nonexistant value
 print(n_sim)
-new_set = n3_4 | n5_9 | set([45,7,4,7,6]) | n_sim
-print(new_set)
-unsorted_list = list(new_set)
-print(unsorted_list)
-sorted_list = sorted(new_set)
-print(sorted_list)
-new_set = set(sorted_list) # going back to set does not guarantee order!
-print(new_set)
-#
-# my_new_set = n3_7.difference(n5_9) # could use -
-# print()
+# new_set = n3_4 | n5_9 | set([45,7,4,7,6]) | n_sim
+# print(new_set)
+# unsorted_list = list(new_set)
+# print(unsorted_list)
+# sorted_list = sorted(new_set)
+# print(sorted_list)
+# new_set = set(sorted_list) # going back to set does not guarantee order!
+# print(new_set)
 # #
-# #
+# # my_new_set = n3_7.difference(n5_9) # could use -
+# # print()
+# # #
+# # #
+# so you can pop remove random item from set
 random_pop = n_sim.pop()  # remove arbitrary element
 print(random_pop, n_sim)
-random_pop = n_sim.pop()
+random_pop = n_sim.pop() # again do not depend on the specific item being removed
 print(random_pop, n_sim)
 my_set_copy = n_sim.copy()
 print(my_set_copy == n_sim, my_set_copy is n_sim) # so True, False because is check for same data location
 my_set_alias = n_sim # this is just a shortcut to original data
 print(my_set_alias == n_sim, my_set_alias is n_sim) # so True and True since we point to same data
-# #
-n_sim.clear() # can clear contents completely
+# # #
+n_sim.clear() # can clear contents completely IN PLACE
 print(n_sim)
 print(my_set_alias) # should be empty, since it points to same set we just cleared
 print(my_set_copy) # still full of data
-# # # # # # # # # # there is also a frozenset which is just like set, but immutable
+# # # # # # # # # # # there is also a frozenset which is just like set, but immutable
--- Day_9_tuples_and_sets/tuples.py
+++ Day_9_tuples_and_sets/tuples.py
@@ -1,13 +1,13 @@
-# # # # # # # # Python Tuples (a,b)
-# # # # # # # # Immutable - size is fixed
-# # # # # # # # Use - passing data that does not need changing
-# # # # # # # # Faster than list - less bookkeeping no worries about size change
-# # # # # # # # "safer" than list
-# # # # # # # # Can be key in dict unlike list
-# # # # # # # # For Heterogeneous data - meaning mixing different data types(int,str,list et al) inside
-# # # # # # # # https://docs.python.org/3/tutorial/datastructures.html#tuples-and-sequences
-# # # # # # # # https://realpython.com/python-lists-tuples/
-# #
+# # # # # # # # # Python Tuples (a,b)
+# # # # # # # # # Immutable - size is fixed
+# # # # # # # # # Use - passing data that does not need changing
+# # # # # # # # # Faster than list - less bookkeeping no worries about size change
+# # # # # # # # # "safer" than list
+# # # # # # # # # Can be key in dict unlike list
+# # # # # # # # # For Heterogeneous data - meaning mixing different data types(int,str,list et al) inside
+# # # # # # # # # https://docs.python.org/3/tutorial/datastructures.html#tuples-and-sequences
+# # # # # # # # # https://realpython.com/python-lists-tuples/
+# # #
 my_tuple = ("Valdis", "programmer", 45, 200.8,
             [10, 20, 30], True, None,
             ('also_tuple', 5, True), {'food': 'potatoes', 'drink': 'kefir'})
@@ -16,43 +16,45 @@ simple_tuple = 10, 20, 30  # This is a tuple
 print(simple_tuple)
 another_tuple = tuple([1, "Valdis", True])  # i need to pass an iterable to tuple constructor
 print(another_tuple)
-print(type(my_tuple))
-numbers_tuple = tuple(range(100, 200, 10))
+numbers_tuple = tuple(range(100, 200, 10)) # 100 to 190 by 10, 200 is not included
 print(numbers_tuple)
 string_tuple = tuple("Valdis")
 print(string_tuple)
-#
-# # regular indexing and slicing rules apply to tuples - just like lists and strings
+# #
+# # # regular indexing and slicing rules apply to tuples - just like lists and strings
 print(my_tuple[0])
 print(len(my_tuple))
 print(my_tuple[-1])  # here also print(my_tuple[7]) would since we have 8 elements starting 0
+print(my_tuple[8]) # same as above
 print(my_tuple[8]['drink'], my_tuple[-1]['drink'], my_tuple[-1].get('drink'))  # get also would work
 print(my_tuple[4])  # list assuming I know the 5th element is list
 print(my_tuple[4][1])  # 2nd item in list
 print(my_tuple[7][1], my_tuple[-2][-2], my_tuple[-2][1])  # all 5s from innner tuple data structure
-# #
+# # #
 mykey = "Valdis", 180  # i can make tuple without parenthesis when there is no confusion
 print(mykey)
 print(type(mykey))
-# # # # # # # to use tuple as key tuple must only have immutables inside as well
+# # # # # # # # to use tuple as key tuple must only have immutables inside as well
 newdict = {mykey: 9000, "secondkey": 9050}
 print(newdict)
 print(newdict[mykey])
 print(newdict[("Valdis", 180)])  # here you will need regular parenthesis to create tuple
 print(newdict["Valdis", 180])  # turns out I can skip the parenthesis
-# #
-# #
-# # my_tuple.
+# # #
+# # #
+# # # my_tuple.
 # my_tuple[0] = "Visvaldis" # error since tuple does not support item assignment
-# #
-# # # # # # # # tuples are immutable but you can mutate those data structures inside which are mutable
+# # #
+# # # # # # # # # tuples are immutable but you can mutate those data structures inside which are mutable
 my_tuple[-1].setdefault(mykey, 500)  # so dict is mutable we can perform in place methods
 print(my_tuple)
-# my_tuple[-1][mykey] = 9000
-# print(my_tuple)
-#
-# # # # += for lists is same as mylist = mylist + [1,2,3]
-# my_tuple[4] += [1,2,3] # i can not write a new list in place of old list, OUT OF PLACE will not work
+my_tuple[-1][mykey] = 9000
+print(my_tuple)
+my_tuple[-1]['drink'] = 'water'
+print(my_tuple)
+# #
+# # # # # += for lists is same as mylist = mylist + [1,2,3]
+# # my_tuple[4] += [1,2,3] # i can not write a new list in place of old list, OUT OF PLACE will not work
 print(my_tuple[4])
 my_tuple[4].extend([1, 2, 3])  # BUT i can can mutate the old list IN PLACE
 print(my_tuple)
@@ -68,16 +70,20 @@ print(my_tuple)
 
 my_tuple[4].clear()
 print(my_tuple)
-# # print(my_tuple[-1])
-# # print(my_tuple)
-# # # # # print(my_tuple[-1].get(mykey))
-# #
-# # # # # # # regular slicing works
+# # # print(my_tuple[-1])
+# # # print(my_tuple)
+# # # # # # print(my_tuple[-1].get(mykey))
+# # #
+
+# # # # # # # # regular slicing works
 mini_3 = my_tuple[:3]  # i can extract sub tuple
 print(mini_3)
 mini_6 = my_tuple[:3] + my_tuple[1:4]  # i can extract sub tuple
 print(mini_6, type(mini_6))
-# # # my_tuple[1] = "teacher" # will not work because tuples are immutable
+# my_tuple[1] = "teacher" # will not work because tuples are immutable
+# i will use a single item tuple below
+print(my_tuple[:1])
+print(my_tuple[2:])
 new_tuple = my_tuple[:1] + ("teacher",) + my_tuple[2:]  # we could slice it together with single item tuple
 print(new_tuple)
 my_list = list(my_tuple)  # i can cast tuple to list
@@ -86,144 +92,149 @@ print(my_list)
 print(my_tuple)
 my_tuple = tuple(my_list)  # i can overwrite reference to old tuple with reference to new tuple
 print(my_tuple)
-# #
-print(numbers_tuple[::2])  # new tuuple of every 2nd value
-print(my_tuple[::-1])
-print(4, 5, "Valdis")  # just some values no tuple
-print((4, 5, "Valdis"))  # so i create tuple on the fly - hot tuple
-# #
-# # # # # # do we have tuple comprehensions?
-# # # # # # not quite but we can use generator expression then cast to tuple
-my_tuple[4].append(50)  # we had an empty list so we use IN PLACE append
+my_tuple[4].append(5000)  # BUT i can can mutate the old list
+# # #
+# print(numbers_tuple[::2])  # new tuuple of every 2nd value
+# print(my_tuple[::-1])
+# print(4, 5, "Valdis")  # just some values no tuple
+# print((4, 5, "Valdis"))  # so i create tuple on the fly - hot tuple
+# # #
+# # # # # # # do we have tuple comprehensions?
+# # # # # # # not quite but we can use generator expression then cast to tuple
+# my_tuple[4].append(50)  # we had an empty list so we use IN PLACE append
 mult_tuple = tuple(el * 2 for el in my_tuple[:6])  # should be faster than list comprehension
 print(mult_tuple)  # so True*2 == 2 because True is 1
-# int_tuple = tuple(
-#     el ** 2 for el in range(1, 10))  # this is not tuple but generator meaning it is made on demand not ready
-# print(int_tuple)
+# # int_tuple = tuple(
+# #     el ** 2 for el in range(1, 10))  # this is not tuple but generator meaning it is made on demand not ready
+# # print(int_tuple)
+# # #
+# # # my_list = []
+# # # # for el in mini_3:
+# # # for el in my_tuple[:5]:
+# # #     if type(el) is int or type(el) is float:
+# # #         my_list.append(1/el) # could also check for zero
+# # #     else: #list or string or something else even
+# # #         my_list.append(el[::-1]) # might want to check also Booleans and None types and dictionaries
+# # # my_rev_tuple = tuple(my_list)
+# # # print(my_rev_tuple)
 # #
-# # my_list = []
-# # # for el in mini_3:
-# # for el in my_tuple[:5]:
-# #     if type(el) is int or type(el) is float:
-# #         my_list.append(1/el) # could also check for zero
-# #     else: #list or string or something else even
-# #         my_list.append(el[::-1]) # might want to check also Booleans and None types and dictionaries
-# # my_rev_tuple = tuple(my_list)
-# # print(my_rev_tuple)
-#
+# Looping over tuples
 for item in my_tuple:
     print(item, "is", type(item))
 # #
-# # # # # # # # # print(my_tuple)
-# # # # # # # # # my_tuple[1] = "scientist"
-# # # # # # # # my_list = list(my_tuple)
-# # # # # # # # print(my_list)
-# # # # # # # # my_list[1] = "scientist"
-# # # # # # # # new_tuple = tuple(my_list)
-# # # # # # # # print(new_tuple)
-# #
-# #
+# # # # # # # # # # print(my_tuple)
+# # # # # # # # # # my_tuple[1] = "scientist"
+# # # # # # # # # my_list = list(my_tuple)
+# # # # # # # # # print(my_list)
+# # # # # # # # # my_list[1] = "scientist"
+# # # # # # # # # new_tuple = tuple(my_list)
+# # # # # # # # # print(new_tuple)
+# # #
+# # #
 t = ()  # empty tuple only question where would you use it? One use to functions which need some sequence
 print(t, type(t))
-# t = (1, 2, 55)  # 2 or more elements
-# print(t, type(t))
+t = (1, 2, 55)  # 2 or more elements, parenthesis are optional
+print(t, type(t))
 single_element_tuple = (5,)  # if you really need a tuple of one element
 print(single_element_tuple, type(single_element_tuple))
-# # # # # # # my_tuple. # i can use . for Intellisense to suggest methods, we only have 2
+# # # # # # # # my_tuple. # i can use . for Intellisense to suggest methods, we only have 2
+
 print(my_tuple.count("programmer"))
-# so tuples have just two methods for external use
+# # so tuples have just two methods for external use
 print(my_tuple.count("teacher"))
 print(my_tuple.index("teacher"))  # returns index of first occurence
 print(my_tuple.index("Valdis"))
 print(my_tuple.index(45))
+# membership test
 if "programmer" in my_tuple:
     print(my_tuple.index("programmer"))
 else:
     print("Not found the key")
-# # # # # # # # # print(my_tuple.index("notprogrammer"))
-# # # # # # # print("somevalue" in my_tuple)
-# #
-# # # # # # # # print(new_tuple.count("programmer"))
-# # # # # # # # print(new_tuple.index("scientist"))
-# # # # # # # # print(my_tuple.index(45))
-# #
-# # # # # # Trick on swapping values
+# # # # # # # # # # print(my_tuple.index("notprogrammer"))
+# # # # # # # # print("somevalue" in my_tuple)
+# # #
+# # # # # # # # # print(new_tuple.count("programmer"))
+# # # # # # # # # print(new_tuple.index("scientist"))
+# # # # # # # # # print(my_tuple.index(45))
+# # #
+# # # # # # # Trick on swapping values
 a = 10
 b = 20
 print(a, b)
-# # how to change them
+# # # how to change them
 temp = a
 a = b
 b = temp
 print(a, b)
-# # # # # # # # # # in Python the above is simpler!
+# # # # # # # # # # # in Python the above is simpler!
 print("Before swap", a, b)
 a, b = b, a  # we can even change a,b,c,d = d,c,b,a and more, so creating tuple on the right and unpack it on the left
 print("After swap", a, b)
-# #
-a, b, c = 5, 10, "Booo"  # example of tuple packing and unpacking
-print(a, b, c)
-# my_num_tuple = 5, 6, 8
-# print(my_num_tuple, type(my_num_tuple))
-# # my_num_tuple = (5, 6, 8) # same as line two lines above
+# # #
+# a, b, c = 5, 10, "Booo"  # example of tuple packing and unpacking
+# print(a, b, c)
+# # my_num_tuple = 5, 6, 8
 # # print(my_num_tuple, type(my_num_tuple))
-# #
-# # # print(my_tuple)
-# # # print(len(my_tuple))
+# # # my_num_tuple = (5, 6, 8) # same as line two lines above
+# # # print(my_num_tuple, type(my_num_tuple))
+# # #
+print(my_tuple)
+print(len(my_tuple))
 name, job, age, top_speed, favorite_list, _, _, inner_tuple, favorite_dict = my_tuple  # tuple unpacking
-# # # # _ symbolizes variables we do not care about
+# # # # # _ symbolizes variables we do not care about
 print(name, job, age, top_speed, favorite_list, inner_tuple, favorite_dict)
-# #
-# # # # extended unpacking
+# # #
+# # # # # extended unpacking
 head, second, *rest, tail = my_tuple  # names you pick yourself *rest will be a list
 print(head, second, rest, tail, sep="\n")
 print(rest, type(rest))
 
 
-#
-# print(my_tuple[0], head)  # that is our head item from tuple
-#
-# tuple_2 = ("Valdis", "RTU")
-# print(tuple_2[0], tuple_2[1])
-# name, school = tuple_2  # unpacking a small 2 item tuple
-# print(name, school)
-#
-# # name, school, bad_val = tuple_2 # will be an error
-# # print(name, school, bad_val)
 # #
-# # # # # # # # name is my_tuple[0]
-# # # # # # # # tuple unpacking and using _ for values that we do need
-# # # # # name, job, _, top_speed, _ = my_tuple[:5]
-# # # # # print(name, _)  # so _ will have value of last unpacking
-# # # # # # (name, job, _, top_speed) = my_tuple[:4]
+# # print(my_tuple[0], head)  # that is our head item from tuple
+# #
+# # tuple_2 = ("Valdis", "RTU")
+# # print(tuple_2[0], tuple_2[1])
+# # name, school = tuple_2  # unpacking a small 2 item tuple
+# # print(name, school)
+# #
+# # # name, school, bad_val = tuple_2 # will be an error
+# # # print(name, school, bad_val)
+# # #
+# # # # # # # # # name is my_tuple[0]
+# # # # # # # # # tuple unpacking and using _ for values that we do need
+# # # # # # name, job, _, top_speed, _ = my_tuple[:5]
 # # # # # # print(name, _)  # so _ will have value of last unpacking
-# #
-# #
+# # # # # # # (name, job, _, top_speed) = my_tuple[:4]
+# # # # # # # print(name, _)  # so _ will have value of last unpacking
+# # #
+# # #
+# tuples are used when returning multiple values from functions
 def get_min_max(my_num_list):
     # so tuple will be created when returning multiple values
     return min(my_num_list), max(my_num_list)  # returns tuple of min and max
 
 
-# #
-# #
+# # #
+# # #
 res = get_min_max([3, 6, 1, 2])
 print(res, type(res))
-# #
-# # # # # i could also unpack immediately the result from tuple into individual values
+# # #
+# # # # # # i could also unpack immediately the result from tuple into individual values
 my_min, my_max = get_min_max([3, 6, 1, 2])
 print(my_min,my_max)
-# #
-# # # # list of tuples can be converted into a dictionary
-# # my_tup_list =[("mykey","Myval"),("anoteherkey",50)] # list of inner lists len 2 would also work
-# # my_new_dict = dict(my_tup_list)
-# # print(my_tup_list)
-# # print(my_new_dict)
-# #
-# # # regular tuples can be hard to use since you need to know what index the item is at
-# # # improvement to regular tuples is namedtuples
-# # # https://docs.python.org/3/library/collections.html#collections.namedtuple
-# # # named tuples give us another access to members using . notation
-# #
-# # print(type(my_tuple[0]) in (list, tuple))
-# # print(type(my_tuple[0]) in (list, tuple, str))
-# #
+# # #
+# # # # # list of tuples can be converted into a dictionary
+my_tup_list =[("mykey","Myval"),("anoteherkey",50)] # list of inner lists len 2 would also work
+my_new_dict = dict(my_tup_list)
+print(my_tup_list)
+print(my_new_dict)
+# # #
+# # # # regular tuples can be hard to use since you need to know what index the item is at
+# # # # improvement to regular tuples is namedtuples
+# # # # https://docs.python.org/3/library/collections.html#collections.namedtuple
+# # # # named tuples give us another access to members using . notation
+# # #
+# # # print(type(my_tuple[0]) in (list, tuple))
+# # # print(type(my_tuple[0]) in (list, tuple, str))
+# # #
