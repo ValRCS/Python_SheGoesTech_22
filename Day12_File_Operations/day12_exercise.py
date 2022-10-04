37f54968f90c0d6cb6bbaa493a2fd9d718e404f6
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-04 20:07:42 +0300

    file cleaning exercise

--- Day12_File_Operations/day12_exercise.py
+++ Day12_File_Operations/day12_exercise.py
@@ -1,33 +1,35 @@
 # Exercise
-# read text from  sherlock_holmes_adventures_1661-0.txt
+# read text from  sherlock_holmes_adventures.txt
 
 # 1a -> write the function file_line_len(fpath), which returns the number of lines in the file
 
-# check file_line_len ("veidenbaums.txt") -> should be 971 or 972
+# check file_line_len("sherlock_holmes_adventures.txt") -> 12305
 
 
 
-# 1b -> write the function get_poem_lines (fpath), which returns a list with only those lines that contain poetry.
+# 1b -> write the function get_text_lines(fpath), which returns a list with only those lines that contain text.
 
-# So we want to avoid/filter out those lines that contain whitespace and also those lines with poem titles.
+# So we want to avoid/filter out those lines that contain whitespace
 
-# PS preferably use encoding = "utf-8" 
+# PS preferably use encoding = "utf-8" when reading 
 
 
 
-# 1c -> write the function save_lines (destpath, lines)
+# 1c -> write the function save_lines(destpath, lines)
 
 # This function will store all lines into destpath file 
 
-# 1d -> call save_lines with destpath being "veid_poems.txt" and lines being the poem lines we cleaned from 1b
+# 1d -> call save_lines with destpath being "pure_sherlock.txt" and lines being the text lines we cleaned from 1b
 
-# 1e -> write the function clean_punkts (srcpath, destpath)
+# 1e -> write the function clean_punkts(srcpath, destpath)
 
 # function will open the srcpath file, clear it from https://docs.python.org/3/library/string.html#string.punctuation
 
 # then function will save the cleaned text into destpath
+# clean_punkts("pure_sherlock.txt", "clean_sherlock.txt")
 
-# 1f -> write the function get_word_usage (srcpath, destpath)
+
+# 1f -> write the function get_word_usage(srcpath, destpath)
 
 # The function opens the file and finds the most frequently used words
 
@@ -37,8 +39,9 @@
 
 # the saved list of words and frequency should be saved in destpath in the following form:
 
-# vards skaits
+# word, count
+# un, 3423
+# es, 3242
 
-# un 3423
-
-# es 3242
+# in effect you will be saving in standard csv format - https://docs.python.org/3/library/csv.html
+# you can use csv module for this, but it is not necessary
