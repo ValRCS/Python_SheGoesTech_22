1600fd037fb84fff66cb22e57882c30da362345b
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-06 20:41:06 +0300

    matplotlib exercise

---
+++ Day_14_ExternalLibraries_CreatingVirtualEnviroment/day14_exercise.py
@@ -0,0 +1,7 @@
+# TODO
+# 1. plot a graph of x and y
+# x would be values from -10 to 10 (21 values)
+# y would be the cube of x + 5
+
+# you could use a different library than matplotlib but it is the most popular one
+# will will explore other ones later on in the course
