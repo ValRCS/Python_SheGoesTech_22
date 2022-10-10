b8b8d08480f8ba03a3d25342f9b2317f57a6c15d
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-10 17:19:08 +0300

    Add Jupyter Notebooks Introduction

---
+++ Day_15_Jupyter_Notebooks/README.py
@@ -0,0 +1,104 @@
+# Jupyter Notebooks
+
+# What are Jupyter Notebooks?
+
+# Jupyter Notebooks are a way to create and share documents that contain live code, 
+# equations, visualizations and narrative text. 
+# Uses include: data cleaning and transformation,
+#  numerical simulation, 
+# statistical modeling,
+# data visualization, 
+# machine learning, 
+# and much more.
+
+# How to install Jupyter Notebooks?
+
+# pip install jupyter
+
+# How to run Jupyter Notebooks?
+
+# jupyter notebook
+
+# How to create a new notebook?
+
+# Click on the New button in the upper right corner of the dashboard.
+
+# How to create a new cell?
+
+# Click on the + button in the upper left corner of the dashboard.
+
+# How to run a cell?
+
+# Click on the Run button in the upper left corner of the dashboard.
+
+# How to change the cell type?
+
+# Click on the Cell Type button in the upper left corner of the dashboard.
+
+# How to change the cell type to Markdown?
+
+# Click on the Cell Type button in the upper left corner of the dashboard and select Markdown.
+
+# How to change the cell type to Code?
+
+# Click on the Cell Type button in the upper left corner of the dashboard and select Code.
+
+
+# how to see variables in the notebook
+
+# %whos
+
+# how to see the documentation of a function
+
+# ?function_name
+
+# how to see the source code of a function
+
+# ??function_name
+
+# how to see the documentation of a module
+
+# ?module_name
+
+# how to see the source code of a module
+
+# ??module_name
+
+# how to see the documentation of a class
+
+# ?class_name
+
+# how to see the source code of a class
+
+# ??class_name
+
+# how to see the documentation of a method
+
+# ?class_name.method_name
+
+# how to see the source code of a method
+
+# ??class_name.method_name
+
+# how to see the documentation of a function
+
+# ?function_name
+
+# how to see the source code of a function
+
+# ??function_name
+
+
+# how to time the execution of a cell
+
+# %time
+
+# how to time the execution of a cell multiple times
+
+# %timeit
+
+# how to time the execution of a function
+
+# %timeit function_name()
+
+
