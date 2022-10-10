7b8d9843c9b4126ebf3353f20c5dcac589080bee
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-10 20:22:08 +0300

    Day15Exercise

---
+++ Day_15_Jupyter_Notebooks/day15_exercise.py
@@ -0,0 +1,20 @@
+# TODO
+# Create a new Jupyter Notebook
+# top cell should be a markdown cell with headline and your name
+# optional insert a hotlink to some nice picture
+
+# rest will be code cells
+# create a new code cell and write a function that prints "Hello Notebooks!"
+# create a code cell for all imports that you need
+# create a code cell for all variables that you need
+# create a code cell for all functions that you need - optional
+# create a code cell for all classes that you need - optional
+# above could also be a single cell with all imports, variables, functions and classes
+
+# have a cell that runs your previous homework from day 14
+# ideally you would see a graph in the notebook
+
+# if you do not have last homework
+# enter any other homework from previous days
+# theoretically you could also enter everything in a single cell
+# but it is better to have a cell for each sub-task
