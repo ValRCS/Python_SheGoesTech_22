22369960c2ae2e0755be04f793db982b1fb2c057
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-11 20:14:37 +0300

    day16 json exercise

---
+++ Day_16_JSON/day16_exercise.py
@@ -0,0 +1,17 @@
+# API stands for Application Programming Interface - 
+# ort of open specification for how to interact with a web service
+# TODO
+# read JSON from some public url
+# some ideas here: https://github.com/public-apis/public-apis
+# also https://jsonplaceholder.typicode.com/
+# https://mockaroo.com/ - requires registration to generate API key
+# choose ones that don't require authentication
+# many APIs are run by volunteers, so they may be slow or unreliable, or down
+# paid APIs are often faster and more reliable
+
+# TODO
+# parse JSON using requests
+# print out the data
+# bonus save the data into a file of some sort, could be JSON could be something else
+
+# you can write using notebook .ipynb or script .py
