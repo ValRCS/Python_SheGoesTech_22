9727b2ecfb73fa3bfd195bf20879189f59da0f16
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-20 22:06:22 +0300

    day20homework

---
+++ Day_20_Web_Automation/day20_homework.py
@@ -0,0 +1,8 @@
+# TODO scrape any car data from ss.com
+# clean up prices as numerics
+# group by make and model
+# plot the data - pricing for sure
+# if you do more advanced analysis that also could be used for the final project
+
+# for those who want scraping part of final project
+# scrape data from somewhere else - maybe Lithuanian ss.com or wikipedia?
