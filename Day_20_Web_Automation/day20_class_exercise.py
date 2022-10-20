c7e33cf560d3bf6b792a3a91eadbff3080702688
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-20 20:26:35 +0300

    Basics of Web Scraping

---
+++ Day_20_Web_Automation/day20_class_exercise.py
@@ -0,0 +1,14 @@
+# TODO in class
+# install BeautifulSoup if you do not have it already
+# pip install beautifulsoup4 
+# note the 4 at the end
+# find two URLs that you want to scrape on SS.com
+# scrape the data from the first page
+# scrape the data from the second page
+# you can use the functions I provided in the class
+
+# save both as csv files
+# and make plot of the data - up to you what you want to plot
+
+
+# homework will be to try to srape date from some other site - such as wikipedia
