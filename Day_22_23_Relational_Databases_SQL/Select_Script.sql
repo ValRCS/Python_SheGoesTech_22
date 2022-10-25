d6f17cdafeb1df634da705e125b6cdb33a842f28
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-25 14:16:51 +0300

    updates sql tutorial

---
+++ Day_22_23_Relational_Databases_SQL/Select_Script.sql
@@ -0,0 +1,17 @@
+SELECT * FROM tracks t;
+-- sqllite tutorial for select is
+-- https://www.sqlitetutorial.net/sqlite-select/
+-- we use -- for comments
+-- Generally you want to limit results
+-- maybe you get 1Million rows
+-- most Database Management systems will limit rows for  you
+-- Dbeaver limits you to 200 by default
+-- as you scroll down you will see more results
+
+-- SQL is case insensitive
+-- Why?
+-- SQL is a standard
+-- SQL was started in the 70's
+-- SQL was created by IBM
+-- not all keyboards had upper case letters or lower case letters in 1970s
+-- so SQL was created to be case insensitive
