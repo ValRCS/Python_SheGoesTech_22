e905e156489f1b381c715465ba9b42882d9f542d
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-25 21:58:23 +0300

    select and joins

--- Day_22_23_Relational_Databases_SQL/Select_Script.sql
+++ Day_22_23_Relational_Databases_SQL/Select_Script.sql
@@ -1,4 +1,4 @@
-SELECT * FROM tracks t;
+--SELECT * FROM tracks t;
 -- sqllite tutorial for select is
 -- https://www.sqlitetutorial.net/sqlite-select/
 -- we use -- for comments
@@ -15,3 +15,70 @@ SELECT * FROM tracks t;
 -- SQL was created by IBM
 -- not all keyboards had upper case letters or lower case letters in 1970s
 -- so SQL was created to be case insensitive
+
+--SELECT * FROM albums a ;
+-- a is just an alias for albums table could be any short text
+-- aliases are used when we join multiple tables
+-- to avoid typing the full table name
+
+--SELECT * FROM albums a
+
+--SELECT 
+--    Title,
+--    Name
+--FROM 
+--    albums
+--INNER JOIN artists 
+--    ON artists.ArtistId = albums.ArtistId;
+
+--SELECT *
+--FROM albums a 
+--JOIN artists a2 
+--ON a.ArtistId = a2.ArtistId;
+
+SELECT * FROM artists a 
+WHERE name LIKE "%Valdis%";
+
+SELECT * FROM albums a 
+WHERE a.ArtistId = 276;
+
+--SELECT
+--*
+--FROM
+--   artists
+--LEFT JOIN albums ON
+--   albums.ArtistId = artists.ArtistId
+--ORDER BY
+--   albums.ArtistId;
+
+--so IF NOT EXISTS will prevent from overwiting old view
+CREATE VIEW IF NOT EXISTS v_rock_songs
+AS 
+SELECT t.Name TrackName, 
+	a.Title  AlbumTitle,
+	a2.Name ArtistName,
+	g.Name Genre,
+	mt.Name MediaType,
+	t.Milliseconds time_ms,
+	t.Milliseconds/1000 seconds,
+	t.Milliseconds/(1000*60) minutes,
+	t.UnitPrice 
+FROM tracks t
+JOIN albums a 
+ON t.AlbumId = a.AlbumId 
+JOIN artists a2 
+ON a.ArtistId = a2.ArtistId 
+JOIN genres g 
+ON t.GenreId = g.GenreId 
+JOIN media_types mt 
+ON t.MediaTypeId = mt.MediaTypeId
+WHERE Genre = "Rock"
+ORDER BY seconds DESC;
+--LIMIT 20 -- so 20 longest rock songs in this database
+--;
+--WHERE a2.Name LIKE "Alanis%";
+
+SELECT * FROM v_rock_songs vrs ;
+
+SELECT * FROM v_rock_songs vrs 
+WHERE ArtistName LIKE "David%";
