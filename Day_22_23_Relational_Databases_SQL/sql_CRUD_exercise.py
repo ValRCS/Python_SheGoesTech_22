7197cab14adcd4a6890912d8b4ac3121d6bc5bde
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-27 20:11:53 +0300

    update database and add exercise

---
+++ Day_22_23_Relational_Databases_SQL/sql_CRUD_exercise.py
@@ -0,0 +1,18 @@
+# TODO: the following 5 functions for CRUD(create, read,update,delete) operations that work with sqlite database called chinook.db
+
+# def create_connection(dbpath):
+#   # can add verbose parameter that prints sqlite version used
+#   return conn
+# def create_artist(conn, artist_name):
+   # use parametrized query !
+#   # do not have to return anything but can use try: inside this function
+# def read_artists(conn):  # can add some extra parameters to limit
+#   return artists  # can return a list of tuples, or you can create a list of artist objects if you want
+# def update_artist(id, new_name):
+# def delete_artist(id=None, name=""):
+#    # provide deletion by id AND/OR name
+
+
+# test it by adding you name to artists table smile
+
+# you can use .ipynb or .py file
