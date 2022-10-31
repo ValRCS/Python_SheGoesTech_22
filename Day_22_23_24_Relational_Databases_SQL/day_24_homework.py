93735cd70e9d7864a4fafddec4243270b454bc88
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-31 21:48:05 +0200

    GROUP BY homework

---
+++ Day_22_23_24_Relational_Databases_SQL/day_24_homework.py
@@ -0,0 +1,12 @@
+# TODO 
+
+# Task 1
+# provide a query that shows total sales by each sales agent
+# will require to join with the customer table
+# will require to join with the invoice table
+
+# Task 2
+# QUERY TO find the top selling track of 2012
+# will require use track table
+# will requiret to join with invoice items table
+# will require to join with the invoice table
