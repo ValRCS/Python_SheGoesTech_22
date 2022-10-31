c50e7278db868cd1b188ae72a9d962d1ca903fea
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-31 20:07:50 +0200

    Chinook GROUP BY exercises

---
+++ Day_22_23_24_Relational_Databases_SQL/day_24_chinook_exercises.py
@@ -0,0 +1,18 @@
+# TODO
+
+# 1. get total sales for all years using invoice table
+# you will want to use SUBSTR to get the year from the invoice date
+# you will want to use SUM to get the total sales for each year
+
+
+# 2. get total sales for each country - use invoice table
+# you will also need to join with the customer table - those have country info
+
+
+# 3. a count tracks in each playlist - use playlist_track table
+
+# 3. b extra challenge get total track lenght in minutes for each playlist
+# you will need to join with the track table
+
+# 3. c cherry on top - provide names of these playlists
+# so you will want to join with the playlist table as well
