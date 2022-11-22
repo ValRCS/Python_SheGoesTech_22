f3c6bc6c28e8f4560828953ced6fb15d736bad6b
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-11-22 20:10:18 +0200

    regex exercise

---
+++ Day_32_Regular_Expressions_Deployment/regex_exercise.py
@@ -0,0 +1,28 @@
+# first lets make a string with some phone numbers from various countries
+
+phone_text = """
++1 555 123 4567
++1 555 123 456
++1 555 123 45
+Latvia +371 20 123 456
+
+Lithuania +370 5 123 456
+
+Estonia +372 5123 456
+
+USA +1 408 123 4567
+
+sometimes we write our phone numbers like this: 555 123 4560
+
+sometimes we write our phone numbers like this: 555-123-4561
+
+sometimes we write our phone numbers like this: 555.123.4562 rarely
+also we use parentheses: (555) 123-4563
+
+so the assignment is to write a regular expression that will match all of these phone numbers
+"""
+
+# TODO get all of these phone numbers using a regular expression
+# and return them as a list
+# see how many you can get
+# of course you can google for some regex ideas but first see how far you can get on your own
